66582c60f200f2b11ce018ca3f941663b6dd9310
Naman Rusia <naman.rusia14@gmail.com>
2025-06-19 18:57:45 -0400

    setting up clients in the backend for db'

---
+++ backend/src/lib/prisma.ts
@@ -0,0 +1,11 @@
+// prisma setup client in globalThis
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
