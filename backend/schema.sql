69a1ec0a0103e9ca1cc11edfe906bfef1cabe6dc
Naman Rusia <naman.rusia14@gmail.com>
2025-07-15 16:40:16 +0530

    trying to fix production

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,1 +1,10 @@
 -- this is the schema for the database. 
+
+generator client {
+  provider      = "prisma-client-js"
+  binaryTargets = ["native", "rhel-openssl-1.0.x"]
+}
+
+datasource db {
+  provider  = "postgresql"
+} 
